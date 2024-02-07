902dadd07de9a55af0af115f42a7abd05a92f587
osama khan <git@osama.email>
2024-02-06 18:22:04 -0800

    added link to blogpost

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -5,7 +5,7 @@ const Header = () => (
   <header className="header">
     <span className="header__icon" role="img" aria-label="keyboard">⌨️</span>
     <h1 className="header__title">hidutil key remapping generator for MacOS Sonoma+</h1>
-    <h6> \\//$ fork deployed from original repo by <a href="https://github.com/amarsyla/hidutil-key-remapping-generator">amarsyla</a> for permanence. hid usage tables and specs can be reviewed <a href='https://developer.apple.com/library/archive/technotes/tn2450/_index.html'>here</a>.</h6>
+    <h6> \$ <a href='https://www.osama.page/keymap'>blog post</a> for context. this tool is a fork deployed (for permanence) from original repo by <a href="https://github.com/amarsyla/hidutil-key-remapping-generator">amarsyla</a>. hid usage tables and specs can be reviewed <a href='https://developer.apple.com/library/archive/technotes/tn2450/_index.html'>here</a>.</h6>
   </header>
 );
 
