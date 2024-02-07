c2d5b57dded956825e3cc7d74a4e65f9a469fe4b
osama khan <git@osama.email>
2024-02-06 18:05:07 -0800

    updated header to give clear credit and change to npm

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -4,7 +4,8 @@ import './header.css';
 const Header = () => (
   <header className="header">
     <span className="header__icon" role="img" aria-label="keyboard">⌨️</span>
-    <h1 className="header__title">hidutil key remapping generator for MacOS</h1>
+    <h1 className="header__title">hidutil key remapping generator for MacOS Sonoma+</h1>
+    <h6>// fork deployed from original repo by <a href="https://github.com/amarsyla/hidutil-key-remapping-generator">amarsyla</a> for permanence. hid usage tables and specs can be reviewed <a href='https://developer.apple.com/library/archive/technotes/tn2450/_index.html'>here</a>.</h6>
   </header>
 );
 
